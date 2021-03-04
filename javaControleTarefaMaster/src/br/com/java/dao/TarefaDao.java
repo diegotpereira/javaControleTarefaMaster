b76cc7a5e3f06b7273118cf01acbdae87ae0acfe
Diego <diegoteixeirapereira@hotmail.com>
2021-03-04 14:32:22 -0300

    criação da classe dao de persistencia de dados

---
+++ javaControleTarefaMaster/src/br/com/java/dao/TarefaDao.java
@@ -0,0 +1,12 @@
+package br.com.java.dao;
+
+import br.com.java.model.Tarefa;
+
+public class TarefaDao {
+
+	public void adiciona(Tarefa tarefa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
