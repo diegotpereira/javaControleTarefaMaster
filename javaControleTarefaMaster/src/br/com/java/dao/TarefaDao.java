71ec52419a02f30680fcc66c04bf2025c9472f47
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-09 11:32:05 -0300

    alteração na classe TarefaDao para adição no banco de dados;

--- javaControleTarefaMaster/src/br/com/java/dao/TarefaDao.java
+++ javaControleTarefaMaster/src/br/com/java/dao/TarefaDao.java
@@ -1,15 +1,38 @@
 package br.com.java.dao;
 
-import javax.xml.ws.RequestWrapper;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import br.com.java.connection.ConnectionFactory;
 import br.com.java.model.Tarefa;
 
 public class TarefaDao {
     
+	private Connection connection;
+	
+	public TarefaDao() {
+		try {
+			connection = ConnectionFactory.getConnection();
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new RuntimeException(e);
+		}
+	}
 	
 	public void adiciona(Tarefa tarefa) {
 		// TODO Auto-generated method stub
-	
+		String sql = "insert into tarefas (descricao, finalizado) values (?,?)";
+//		PreparedStatement stmt;
+		try {
+//			stmt = connection.prepareStatement(sql);
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, tarefa.getDescricao());
+			stmt.setBoolean(2, tarefa.isFinalizado());
+			stmt.execute();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
