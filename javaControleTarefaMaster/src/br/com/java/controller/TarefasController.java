f6c48bd98cf96951519b2eb898435e6fac171dc6
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-10 09:00:35 -0300

    alteração nos arquivos classe java

--- javaControleTarefaMaster/src/br/com/java/controller/TarefasController.java
+++ javaControleTarefaMaster/src/br/com/java/controller/TarefasController.java
@@ -1,11 +1,14 @@
 package br.com.java.controller;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.java.dao.TarefaDao;
 import br.com.java.model.Tarefa;
@@ -33,5 +36,29 @@ public class TarefasController {
 		model.addAttribute("tarefas", dao.getListaTarefa());
 		return "tarefa/lista";
 	}
-	
+	@RequestMapping("removeTarefa")
+	public String remove(Tarefa tarefa, HttpServletResponse response) {
+		TarefaDao dao = new TarefaDao();
+		dao.remove(tarefa);
+		response.setStatus(200);
+		return "redirect:listaTarefa";	
+	}
+	@RequestMapping("alteraTarefa")
+	public String altera(Tarefa tarefa) {
+		TarefaDao dao = new TarefaDao();
+		dao.altera(tarefa);
+		return "redirect:listaTarefa";
+	}
+	@RequestMapping("mostraTarefa")
+	public String mostra(Long id, Model model) {
+		TarefaDao dao = new TarefaDao();
+		model.addAttribute("tarefa", dao.buscaPorId(id));
+		return "tarefa/mostra";
+	}
+	@ResponseBody
+	@RequestMapping("finalizaTarefa")
+	public void finalizar(Long id) {
+		TarefaDao  dao = new TarefaDao();
+		dao.finaliza(id);
+	}
 }
--- javaControleTarefaMaster/src/br/com/java/dao/TarefaDao.java
+++ javaControleTarefaMaster/src/br/com/java/dao/TarefaDao.java
@@ -1,6 +1,7 @@
 package br.com.java.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.DateFormat;
@@ -76,5 +77,93 @@ public class TarefaDao {
 			throw new RuntimeException(e);
 		}
 	}
-
+	public void remove(Tarefa tarefa) {
+		
+		if (tarefa.getId() == null) {
+			throw new IllegalStateException("Id da tarefa não pode ser nulo.");
+		}
+		try {
+			String sql = "DELETE FROM tarefas where id = ?";
+			
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			
+			stmt.setLong(1, tarefa.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new RuntimeException(e);
+		}
+	}
+	public void altera(Tarefa tarefa) {
+		String sql = "UPDATE tarefas SET descricao = ?, finalizado = ?, dataFinalizacao = ? WHERE id = ?";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, tarefa.getDescricao());
+			stmt.setBoolean(2, tarefa.isFinalizado());
+			stmt.setDate(3, tarefa.getDataFinalizacao() != null ? new Date(tarefa.getDataFinalizacao().getTimeInMillis()): null);
+			stmt.setLong(4, tarefa.getId());
+			stmt.execute();
+		} catch (SQLException e) {
+			// TODO: handle exception
+			throw new RuntimeException(e);
+		}
+	}
+	public Tarefa buscaPorId(Long id) {
+		
+		String sql = "SELECT * FROM tarefas WHERE id = ?";
+		if (id == null) {
+			throw new IllegalStateException("id da tarefa não deve ser nulo.");
+		}
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setLong(1, id);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			if (rs.next()) {
+				return populaTarefa(rs);
+			}
+			rs.close();
+			stmt.close();
+			
+			return null;
+		} catch (SQLException e2) {
+			// TODO: handle exception
+			throw new RuntimeException(e2);
+		}
+	}
+	private Tarefa populaTarefa(ResultSet rs) throws SQLException{
+		Tarefa tarefa = new Tarefa();
+		
+		tarefa.setId(rs.getLong("id"));
+		tarefa.setDescricao(rs.getString("descricao"));
+		tarefa.setFinalizado(rs.getBoolean("finalizado"));
+		
+		Date data = rs.getDate("dataFinalizacao");
+		
+		if (data != null) {
+			Calendar dataFinalizacao = Calendar.getInstance();
+			dataFinalizacao.setTime(data);tarefa.setDataFinalizacao(dataFinalizacao);
+		}
+		return tarefa;
+	}
+	public void finaliza(Long id) {
+		String sql = "UPDATE tarefas SET finalizado = ?, dataFinalizacao = ? WHERE id = ?";
+		
+		if (id == null) {
+			throw new IllegalStateException("Id da tarefa não deve ser nulo.");
+		}
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setBoolean(1, true);
+			stmt.setDate(2, new Date(Calendar.getInstance().getTimeInMillis()));
+			stmt.setLong(3, id);
+			stmt.execute();
+		} catch (SQLException e) {
+			// TODO: handle exception
+			throw new RuntimeException(e);
+		}
+	}
 }
